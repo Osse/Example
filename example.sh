050ecdc3a9a674a96ff294bcaba20695b0f38d62
Øystein Walle <ow@ceragon.com>
2012-08-30 11:24:29 +0200

    Initial commit

---
+++ example.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [[ -z "$3" ]]
+then
+        echo "Max two arguments"
+        if [[ -z "$2" ]]
+        then
+                echo "Max one argument"
+                if [[ -z "$1" ]]
+                then
+                        echo "Zero arguments"
+                fi
+        fi
+fi
